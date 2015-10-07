2577a824f1f9eea18ae768ad1b6f349952b7a9be
apopma <adam.popma@gmail.com>
2015-10-06 17:39:32 -0700

    write divisible_by and main print loop

--- fizzbuzzonacci.py
+++ fizzbuzzonacci.py
@@ -2,6 +2,20 @@ def fizzbuzzonacci(n):
     # First, generate the first n Fibonacci numbers.
     fibos = fibonacci_numbers(n)
 
+    # Then, print lines based on their Fizz/Buzz/BuzzFizz status.
+    # This implementation will print multiple things if multiple conditions obtain,
+    # i.e. it prints "Buzz" and then "Fizz" (on separate lines) if a number is divisible by 3 and 5.
+    for fibo in fibos:
+        if prime(fibo):
+            print "BuzzFizz"
+            continue
+        elif divisible_by(fibo, 3):
+            print "Buzz"
+        elif divisible_by(fibo, 5):
+            print "Fizz"
+        else:
+            print fibo
+
 def fibonacci_numbers(num):
     # I'm considering the Fibonacci sequence to begin with the terms 1, 1, 2, 3...
     # where the zeroth fibonacci number is meaningless, and the first is 1.
@@ -14,7 +28,9 @@ def fibonacci_numbers(num):
     return fibos
 
 def divisible_by(num, divisor):
+    return num % divisor == 0
+
+def prime(num):
     pass
 
-def is_prime(num):
-    pass
+fizzbuzzonacci(10)
