c145b4cb796ecdeae3a88719bf5ad470eedb7637
apopma <adam.popma@gmail.com>
2015-10-06 17:25:09 -0700

    generate fibonacci numbers

--- fizzbuzzonacci.py
+++ fizzbuzzonacci.py
@@ -1,8 +1,17 @@
 def fizzbuzzonacci(n):
-    pass
+    # First, generate the first n Fibonacci numbers.
+    fibos = fibonacci_numbers(n)
 
 def fibonacci_numbers(num):
-    pass
+    # I'm considering the Fibonacci sequence to begin with the terms 1, 1, 2, 3...
+    # where the zeroth fibonacci number is meaningless, and the first is 1.
+    if num == 0: return []
+    if num == 1: return [1]
+
+    fibos = [1, 1]
+    while len(fibos) < num:
+        fibos.append(fibos[-1] + fibos[-2])
+    return fibos
 
 def divisible_by(num, divisor):
     pass
