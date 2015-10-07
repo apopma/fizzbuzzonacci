47fd9c9f5a3969949186f3da57b1439a885895a8
apopma <adam.popma@gmail.com>
2015-10-07 11:22:35 -0700

    write doctests and refactor

--- fizzbuzzonacci.py
+++ fizzbuzzonacci.py
@@ -1,26 +1,53 @@
 import math
 
 def fizzbuzzonacci(n):
-    # First, generate the first n Fibonacci numbers.
-    fibos = fibonacci_numbers(n)
+    """
+    Generate the first n Fibonacci numbers and print their Fizz/Buzz status:
+    "BuzzFizz" if prime, "Buzz" if divisible by 3, and "Fizz" if divisible by 5.
+    Multiple lines will be printed if a number meets multiple conditions;
+    i.e. 3 will print BuzzFizz and Buzz: it is prime and divisible by 3.
 
-    # Then, print lines based on their Fizz/Buzz/BuzzFizz status.
-    # This implementation will print multiple things if multiple conditions obtain,
-    # i.e. it prints "Buzz" and then "Fizz" (on separate lines) if a number is divisible by 3 and 5.
+    >>> fizzbuzzonacci(10)
+    1
+    1
+    BuzzFizz
+    BuzzFizz
+    Buzz
+    BuzzFizz
+    Fizz
+    8
+    BuzzFizz
+    Buzz
+    34
+    Fizz
+    """
+
+    if type(n) is not int: return None
+
+    fibos = fibonacci_numbers(n)
     for fibo in fibos:
-        if prime(fibo):
-            print "BuzzFizz"
-            continue
-        elif divisible_by(fibo, 3):
-            print "Buzz"
-        elif divisible_by(fibo, 5):
-            print "Fizz"
-        else:
+        prime = is_prime(fibo)
+        mod3 = divisible_by(fibo, 3)
+        mod5 = divisible_by(fibo, 5)
+
+        if prime: print "BuzzFizz"
+        if mod3:  print "Buzz"
+        if mod5:  print "Fizz"
+
+        if (prime or mod3 or mod5) is False:
             print fibo
 
 def fibonacci_numbers(num):
-    # I'm considering the Fibonacci sequence to begin with the terms 1, 1, 2, 3...
-    # where the zeroth fibonacci number is meaningless, and the first is 1.
+    """
+    Generate an array of the first 'num' Fibonacci numbers, starting with [1, 1, 2...]
+    Returns blank array for input less than 1.
+
+    >>> fibonacci_numbers(10)
+    [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    >>> fibonacci_numbers(0)
+    []
+    """
+
     if num == 0: return []
     if num == 1: return [1]
 
@@ -30,9 +57,36 @@ def fibonacci_numbers(num):
     return fibos
 
 def divisible_by(num, divisor):
+    """
+    Returns a boolean: is 'num' divisible by 'divisor' without any remainder?
+    Returns False if divisor is 0 instead of raising a ZeroDivisionError.
+    >>> divisible_by(100, 10)
+    True
+    >>> divisible_by(25, 5)
+    True
+    >>> divisible_by(17, 6)
+    False
+    >>> divisible_by(100, 0)
+    False
+    """
+
+    if divisor is 0: return False
     return num % divisor == 0
 
-def prime(n):
+def is_prime(n):
+    """
+    Checks whether or not an integer n is prime. Adapted from 'euler/euler10',
+    which was itself adapted from a Stack Overflow answer.
+    >>> is_prime(0)
+    False
+    >>> is_prime(2)
+    True
+    >>> is_prime(100)
+    False
+    >>> is_prime(7919)
+    True
+    """
+
     if n <= 1: return False
     if n == 2: return True
     if n % 2 == 0: return False
@@ -43,4 +97,6 @@ def prime(n):
             return False
     return True
 
-fizzbuzzonacci(10)
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
